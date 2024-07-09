0f7c326791a4d215a866a5f6c03c41e9409cb507
Alexander Ermolenko <ermolenko@nethouse.ru>
2024-07-09 20:56:54 +0300

    hw 07 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,50 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	st, err := os.Stat(fromPath)
+	if err != nil {
+		return fmt.Errorf("failed to get stat %s with error %s", fromPath, err.Error())
+	}
+
+	if st.IsDir() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > st.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	fileFrom, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s, %s", fromPath, err.Error())
+	}
+	defer fileFrom.Close()
+
+	if offset > 0 {
+		_, err := fileFrom.Seek(offset, 0)
+		if err != nil {
+			return fmt.Errorf("failed to seek file %s", err.Error())
+		}
+	}
+
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("failed to create temp file %s, %s", toPath+"/"+fileFrom.Name(), err.Error())
+	}
+	defer fileTo.Close()
+
+	toRead := st.Size() - offset
+	if limit > 0 && limit < toRead {
+		toRead = limit
+	}
+	reader := io.LimitReader(fileFrom, toRead)
+	bar := pb.Full.Start64(toRead)
+	barReader := bar.NewProxyReader(reader)
+	_, err = io.CopyN(fileTo, barReader, toRead)
+	if err != nil {
+		return fmt.Errorf("failed to copy %d bytes with error %s", toRead, err.Error())
+	}
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -3,5 +3,43 @@ package main
 import "testing"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{
+			name: "unsupported file if isDir",
+			args: args{
+				fromPath: "testdata",
+				toPath:   "",
+				offset:   0,
+				limit:    0,
+			},
+			wantErr: ErrUnsupportedFile,
+		},
+		{
+			name: "offset exceeds file size",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "out.txt",
+				offset:   9999999,
+				limit:    0,
+			},
+			wantErr: ErrOffsetExceedsFileSize,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit); err != tt.wantErr {
+				t.Errorf("Copy() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
