f336921fda43ffff7508279100a9a272549888ac
Alexander Ermolenko <ermolenko@nethouse.ru>
2024-07-09 22:52:11 +0300

    hw 08 done

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +19,45 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	st, err := os.Stat(dir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get stat %s with error %s", dir, err.Error())
+	}
+
+	if !st.IsDir() {
+		return nil, errors.New("expected dir")
+	}
+
+	dirEntries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read dir %s with error %s", dir, err.Error())
+	}
+
+	env := make(Environment)
+	for _, dirEntry := range dirEntries {
+		filepath := dir + "/" + dirEntry.Name()
+		envVar := strings.ReplaceAll(dirEntry.Name(), "=", "")
+
+		file, err := os.Open(filepath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open file %s with err %s", filepath, err.Error())
+		}
+		defer file.Close()
+
+		env[envVar] = EnvValue{
+			NeedRemove: true,
+		}
+
+		scanner := bufio.NewScanner(file)
+		if scanner.Scan() {
+			content := strings.ReplaceAll(scanner.Text(), "\x00", "\n")
+			content = strings.TrimRight(content, " \t")
+			env[envVar] = EnvValue{
+				Value:      content,
+				NeedRemove: false,
+			}
+		}
+	}
+
+	return env, nil
 }
