5ba9b828f753084823e836773373f171ca986ee5
Alexander Ermolenko <ermolenko@nethouse.ru>
2024-07-11 23:32:31 +0300

    fix lint

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -25,8 +25,7 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 		}
 	}
 
-	command := exec.Command(cmd[0], cmd[1:]...)
-	// #nosec
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
 	command.Stdout = os.Stdout
 	command.Stdin = os.Stdin
 	command.Stderr = os.Stderr
